c3f7ee8623f7b484d0cc1356982800ccd47c0961
Chamidu Lakshan <151443855+lakshanchamidu@users.noreply.github.com>
2024-05-16 20:18:31 +0530

    Add files via upload

---
+++ First/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    age = 40;
+    printf("My age is %d years old.\n",age);
+    age = age - 10;
+    printf("Age is %d years old.\n",age);
+    return 0;
+}
