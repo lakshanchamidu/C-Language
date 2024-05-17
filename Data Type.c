62e2c48b513964911da17fdacf0deb1e9baa62d8
Chamidu Lakshan <151443855+lakshanchamidu@users.noreply.github.com>
2024-05-17 20:28:19 +0530

    Add files via upload

---
+++ Data Type.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a = 2;
+    printf("%d \n" , a );
+    char first = 'i';
+    printf("%c  \n" , first);
+    float b = 21.65;
+    printf("%f  \n", b);
+    return 0;
+}
