5dbefd95d74639938694682286ccf25470b531af
Chamidu Lakshan <151443855+lakshanchamidu@users.noreply.github.com>
2024-05-18 15:01:32 +0530

    get inputs

--- First/main.c
+++ First/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-    int age;
-    age = 40;
-    printf("My age is %d years old.\n",age);
-    age = age - 10;
-    printf("Age is %d years old.\n",age);
+    int a  = 12;
+    printf("System given number is: %d\n", a);
+    printf("Enter your number: ");
+    scanf("%d", &a);
+    printf("You have enter the number is: %d\n", a);
     return 0;
 }
